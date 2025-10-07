8d43d5ac0a88c608081b8b7d5b31a2d349363312
Matthew Kight <m.kight28@gmail.com>
2025-10-07 08:46:01 -0400

    Closes #70, Closes #71, Closes #73: Modified JSON structure to match new project structure, Improved DataLoader with helper methods and match new JSON structure.

--- src/main/java/com/escape/code/DataLoader.java
+++ src/main/java/com/escape/code/DataLoader.java
@@ -33,65 +33,14 @@ public class DataLoader extends DataConstants {
                 JSONObject personJSON = (JSONObject)peopleJSON.get(i);
                 String userName = (String)personJSON.get(USER_USER_NAME);
                 String password = (String)personJSON.get(USER_PASSWORD);
-
-                //User Settings JSON Object
-                JSONObject settingsJSON = (JSONObject)personJSON.get(USER_SETTINGS);
-                int volume =  ((Long)settingsJSON.get(USER_SETTINGS_VOLUME)).intValue();
-                int difficulty =  ((Long)settingsJSON.get(USER_SETTINGS_DIFFICULTY)).intValue();
-                Settings settings = new Settings(volume, difficulty);
-
-                //Progress JSON Object
-                JSONObject progressJSON = (JSONObject)personJSON.get(USER_PROGRESS);
-                int cluesUsed = ((Long)progressJSON.get(USER_CLUES_USED)).intValue();
-
-                //Puzzles Solved in Progress Object
-                JSONArray puzzles = (JSONArray)progressJSON.get(USER_PUZZLES_SOLVED);
-                HashMap<String, Puzzle> puzzleMap = new HashMap<>();
-                for(int j=0; j<puzzles.size(); j++) {
-                    JSONObject puzzleJSON = (JSONObject)puzzles.get(j);
-                    String key = (String)puzzleJSON.get(USER_PUZZLE_HASH_KEY);
-                    JSONObject value = (JSONObject)puzzleJSON.get(USER_PUZZLE_HASH_VAL);
-                    String description = (String)value.get(USER_PUZZLE_DESC);
-                    ArrayList<String> hints = new ArrayList<>();
-                    JSONArray hintsJSON = (JSONArray)value.get(USER_PUZZLE_HINTS);
-                    for(int k=0; k<hintsJSON.size(); k++) {
-                        String hint = (String)hintsJSON.get(k);
-                        hints.add(hint);
-                    }
-                    String solution = (String)value.get(USER_PUZZLE_SOLUTION);
-                    boolean isSolved = (boolean)value.get(USER_PUZZLE_SOLVED);
-                    Puzzle puzzle = new Puzzle(description, hints, solution, isSolved);
-                    puzzleMap.put(key, puzzle);
-                }
-
-                //Progress Achievements JSON Array
-                JSONArray achievementsJSON = (JSONArray)progressJSON.get(USER_ACHIEVEMENTS);
-                ArrayList<Achievement> achievements = new ArrayList<>();
-                for(int j=0; j<achievementsJSON.size(); j++) {
-                    JSONObject achievementJSON = (JSONObject)achievementsJSON.get(j);
-                    String title = (String)achievementJSON.get(USER_ACHIEVEMENT_TITLE);
-                    boolean unlocked = (boolean)achievementJSON.get(USER_ACHIEVEMENT_UNLOCKED);
-                    Achievement achievement = new Achievement(title, unlocked);
-                    achievements.add(achievement);
-
-                }
-
-                //Rooms JSON Array
-                JSONArray roomsJSON = (JSONArray)personJSON.get(USER_ROOMS);
-                ArrayList<UUID> rooms = new ArrayList<>();
-                for(int j=0; j<roomsJSON.size(); j++) {
-                    UUID room = UUID.fromString((String)roomsJSON.get(j));
-                    rooms.add(room);
-                }
-
-                //Current Room
-                UUID currentRoom = UUID.fromString((String)personJSON.get(USER_CURRENT_ROOM));
-
-                //Progress Object construction
-                Progress progress = new Progress(currentRoom, puzzleMap, cluesUsed, achievements);
+                Settings settings = loadSettings(personJSON);
+                RoomList roomlist = RoomList.getInstance();
+                ArrayList<Room> rooms = loadRooms(personJSON);
+                UUID currentRoomID = UUID.fromString((String)personJSON.get(USER_CURRENT_ROOM));
+                Room currentRoom = RoomList.getRoom(currentRoomID);
 
                 //Create User Object & add to list
-                User user = new User(userName, password, settings, progress, rooms, currentRoom);
+                User user = new User(userName, password, settings, rooms, currentRoom);
                 users.add(user);
             }
             reader.close();
@@ -121,22 +70,11 @@ public class DataLoader extends DataConstants {
                 UUID id = UUID.fromString((String)roomJSON.get(ROOM_ID));
                 int timer = ((Long)roomJSON.get(ROOM_TIMER)).intValue();
                 int difficulty = ((Long)roomJSON.get(USER_SETTINGS_DIFFICULTY)).intValue();
-
-                //Get Room Map JSON Object
                 Map map = loadMap(roomJSON);
-
-                //Progress List
                 HashMap<String, Progress> progressList = loadProgressList(roomJSON);
-
-                //Progress Object
                 Progress progress = loadProgress(roomJSON);
-
-                //Get Room Puzzles JSON Array
                 ArrayList<Puzzle> puzzles = loadPuzzles(roomJSON);
-
-                //Get Room Leaderboard Object
                 Leaderboard leaderboard = loadLeaderboard(roomJSON);
-
 
                 Room room = new Room(id, map, progressList, progress, leaderboard, puzzles, timer, difficulty);
                 rooms.add(room);
@@ -205,6 +143,7 @@ public class DataLoader extends DataConstants {
 
     private static ArrayList<Room> loadRooms(JSONObject parentJSON) {
         //List of Rooms for a User
+        RoomList roomlist = RoomList.getInstance();
         JSONArray userRoomsJSON = (JSONArray)parentJSON.get(USER_ROOMS);
         ArrayList<Room> roomList = new ArrayList<>();
         for(int i=0; i<userRoomsJSON.size(); i++) {
@@ -277,10 +216,10 @@ public class DataLoader extends DataConstants {
     }
 
     public static void main(String[] args) {
-        ArrayList<Room> rooms = DataLoader.getRooms();
+        ArrayList<User> users = DataLoader.getUsers();
 
-        for(Room room : rooms) {
-            System.out.println(room);
+        for(User user : users) {
+            System.out.println(user);
         }
     }
 
