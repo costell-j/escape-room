15bdeda552c89379fabcc4d8dd09eefb1116db4b
Costell Johnson <davaunjohnson0@gmail.com>
2025-10-21 09:08:35 -0400

    Closes #98, Closes #99, Closes #90, Closes #88, Closes #43, Closes #44: I added JavaDoc to Room, and noticed some tasks I forgot to close

--- src/main/java/com/escape/code/Room.java
+++ src/main/java/com/escape/code/Room.java
@@ -23,12 +23,12 @@ public class Room {
      * @param id Room's UUID
      * @param name Name of the room
      * @param map Room's map
-     * @param progressList Room's pr
-     * @param progress
-     * @param leaderboard
-     * @param puzzles
-     * @param timer
-     * @param difficulty
+     * @param progressList Room's progress list
+     * @param progress Room's progress object
+     * @param leaderboard Room's leaderboard
+     * @param puzzles Room's array of puzzles
+     * @param timer Room's timer
+     * @param difficulty Room's diffficulty
      */
     public Room(UUID id, String name, Map map, HashMap<String, Progress> progressList, Progress progress, Leaderboard leaderboard, ArrayList<Puzzle> puzzles, int timer, int difficulty) {
         this.id = id;
@@ -44,30 +44,58 @@ public class Room {
 
     // Getters
 
+    /**
+     * Getter for the room's name
+     * @return name
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Getter for the room's progress list
+     * @return progress list
+     */
     public HashMap<String, Progress> getProgressList() {
         return this.progressList;
     }
 
+    /**
+     * Getter for room's progress
+     * @return progress
+     */
     public Progress getProgress() {
         return this.progress;
     }
 
+    /**
+     * Gettter for room's map
+     * @return map
+     */
     public Map getMap() {
         return this.map;
     }
 
+    /**
+     * Getter for room's leaderboard
+     * @return leaderboard
+     */
     public Leaderboard getLeaderboard() {
         return this.leaderboard;
     }
 
+    /**
+     * Getter for the timer
+     * @return timer
+     */
     public int getTimer() {
         return this.timer;
     }
 
+    /**
+     * Getter for room's puzzles
+     * @return puzzles
+     */
     public ArrayList<Puzzle> getPuzzles() {
         if(puzzles == null)
             this.puzzles = new ArrayList<Puzzle>();
@@ -76,29 +104,57 @@ public class Room {
         return this.puzzles;
     }
 
+    /**
+     * Getter for room's difficulty
+     * @return difficulty
+     */
     public int getDifficulty() {
         return this.difficulty;
     }
 
+    /**
+     * Getter for the room's UUID
+     * @return id
+     */
     public UUID getId() {
         return this.id;
     }
 
     // Setters
 
+    /**
+     * Setter for the room's timer
+     * @param timer the room's time
+     */
     public void setTime(int timer) {
         this.timer = timer;
     }
 
+    /**
+     * Setter for the room's difficulty
+     * Restricts the difficulty to 1-3, defaulted at 1
+     * @param difficulty the room's difficulty
+     */
     public void setDifficulty(int difficulty) {
         if(difficulty > 0 && difficulty < 4)
-        this.difficulty = difficulty;
+            this.difficulty = difficulty;
+        else
+            this.difficulty = 1;
     }
 
+    /**
+     * Setter for the room's leaderboard
+     * @param leaderboard the room's leaderboard
+     */
     public void setLeaderboard(Leaderboard leaderboard) {
         this.leaderboard = leaderboard;
     }
 
+    /**
+     * Setter for Progress based on username
+     * If there is no Progress assigned to that username, one is made
+     * @param username the user's username
+     */
     public void setProgress(String username) {
         for(String s : progressList.keySet()) {
             if(s.equals(username)) {
@@ -111,20 +167,33 @@ public class Room {
         }
     }
 
+    /**
+     * Base setter the room's progress
+     * @param progress the room's progress
+     */
     public void setProgress(Progress progress) {
         this.progress = progress;
     }
     
     // Functionality Methods
 
+    /**
+     * Allows the user to play the current puzzle
+     */
     public void playPuzzle() {
         puzzles.get(this.progress.getCurrentPuzzle());
     }
 
+    /**
+     * Advances the user onto the next puzzle
+     */
     public void advancePuzzle() {
         this.progress.setCurrentPuzzle(this.progress.getCurrentPuzzle()+1);
     }
     
+    /**
+     * toString for the Room
+     */
     @Override
     public String toString() {
         String desc = "ID: "+this.id+"\nMap: "+this.map.toString()+"\nLeaderboard: "+this.leaderboard.toString();
@@ -134,6 +203,11 @@ public class Room {
         }
         return desc;
     }
+
+    /**
+     * Method to adjust timer based on difficulty
+     * @param difficulty room's difficulty
+     */
     public void timeChange(int difficulty) {
         int time = switch(difficulty) {
             case 1 -> 1200;
@@ -143,6 +217,11 @@ public class Room {
         };
         setTime(time);
     }
+
+    /**
+     * Method that formats the timer into a min:secs
+     * @return the formatted timer
+     */
     public String formatTimer() {
         String formattedTimer;
         int timer = getTimer();
