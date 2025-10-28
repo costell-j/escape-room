b368d2be25e7621f2979b3c243168f1ffe561cc9
Matthew Kight <m.kight28@gmail.com>
2025-10-28 09:30:46 -0400

    Setup testing suite, completely

---
+++ src/test/java/com/escape/code/TestTesting.java
@@ -0,0 +1,12 @@
+package com.escape.code;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TestTesting {
+    @Test
+    public void testTesting() {
+        assertTrue(true);
+    }
+}
+
