213b9b3505dbe12bd5e4b36958784226b02ed958
Costell Johnson <davaunjohnson0@gmail.com>
2025-10-30 11:58:43 -0400

    Added my test files and started testing Settings

---
+++ src/test/java/com/escape/code/ProgressTests.java
@@ -0,0 +1,12 @@
+package com.escape.code;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProgressTests {
+    
+}
