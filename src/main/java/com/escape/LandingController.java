e9395e43848737c9d8f7f03d1b96c90f6a4b7764
bsumpter7 <bsumpter@email.sc.edu>
2025-11-11 09:44:21 -0500

    .

--- src/main/java/com/escape/LandingController.java
+++ src/main/java/com/escape/LandingController.java
@@ -2,8 +2,19 @@ package com.escape;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class LandingController {
+    @FXML
+    private Label welcomeLabel;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private Button exitButton;
+    
     @FXML
     private void switchToLogin() throws IOException {
         App.setRoot("login");
