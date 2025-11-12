805957fa025791887f7902e2cdb32d8c66a1e9eb
Costell Johnson <davaunjohnson0@gmail.com>
2025-11-11 23:38:01 -0500

    Login controller finished

--- src/main/java/com/escape/LoginController.java
+++ src/main/java/com/escape/LoginController.java
@@ -1,12 +1,42 @@
 package com.escape;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
 
+import com.escape.code.*;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+
+/**
+ * @author Costell Johnson
+ */
 public class LoginController {
+    @FXML
+    private TextField txt_username;
+    @FXML
+    private TextField txt_password;
+    @FXML
+    private Label lbl_error;
 
     @FXML
-    private void switchToLogin() throws IOException {
+    private void btnLoginClicked(MouseEvent event) throws IOException {
+        String username = txt_username.getText();
+        String password = txt_password.getText();
+
+        GameManager gameManager = new GameManager();
+
+        if(!gameManager.login(username, password)) {
+            lbl_error.setText("Invalid username or paassword");
+            return;
+        }
+        App.setRoot("user_home");
+    }
+
+    @FXML
+    private void back(MouseEvent event) throws IOException {
         App.setRoot("landing");
     }
 }
